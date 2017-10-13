87a26982b5bc96ab841fbf0c380eb25ffbd3915e
jroyals2 <jamesroyals3@gmail.com>
2017-10-13 14:21:14 -0400

    Added a DB for the models

---
+++ db/schema.js
@@ -0,0 +1,42 @@
+const mongoos = require('mongoose')
+
+// This will be the items schema nested in the Pantry Schema
+const ItemSchema = mongoose.Schema({
+    itemName: {
+        type: String,
+        required: [true, "Must have a name"]
+    },
+    quantity: Number,
+    parLevel: Number
+
+})
+// This will be the schema for the pantry nested inside the user schema
+const PantrySchema = mongoose.Schema({
+    pantryName: {
+        type: String,
+        required: true
+    },
+    location: String,
+    items: [ItemSchema]
+})
+// This will be the master schema 
+const UserSchema = mongoose.Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+    },
+    pantry: [PantrySchema]
+
+})
+
+const Item = mongoose.model(Item, 'ItemScheman')
+const Pantry = mongoose.model(Pantry, 'PantrySchema')
+const User = mongoose.model(User, 'UserSchema')
+
+
+module.exports= {
+    Item, Pantry, User
+}
