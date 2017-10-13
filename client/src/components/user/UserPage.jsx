df0472338f1b5af9dbd0c26efab414cb084e3cf9
jroyals2 <jamesroyals3@gmail.com>
2017-10-13 16:32:48 -0400

    setup the display for the user info

--- client/src/components/user/UserPage.jsx
+++ client/src/components/user/UserPage.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 class UserPage extends Component {
+    state = {
+        user : {
+            userName:'',
+            password: '',
+            pantry: []
+        }
+    }
+
+    async componentWillMount(){
+        const { userId } = this.props.match.params
+        const res = await axios.get(`/api/users/${userId}`)
+        this.setState({user: res.data})
+    }
+
     render() {
         return (
             <div>
-                <h1>Hello from the User Page!</h1>
+                <h1>{this.state.user.userName}</h1>
+                {this.state.user.pantry.map((pantry) => {
+                    return (
+                        <Link key={pantry._id} to={`/user/${this.state.user._id}/pantry/${pantry._id}`}>{pantry.pantryName}</Link>
+                    )
+                })}
             </div>
         );
     }
