8dfffd056e28e1a588761526084ec3f0269f2ca8
jroyals2 <jamesroyals3@gmail.com>
2017-10-13 15:18:20 -0400

    added a get route for the users

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
+const usersController = require('./routes/UsersController');
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI); //mongodb://localhost/idea-board
 
@@ -17,6 +18,7 @@ connection.on('error', (err) => {
 }); 
 app.use(express.static(`${__dirname}/client/build`))
 app.use(bodyParser.json());
+app.use('/api/users', usersController)
 app.get('/', (req,res) => {
   res.sendFile(`${__dirname}/client/build/index.html`)
 })
