9e864ed80db44da75d3ba8ade32320875d408726
jroyals2 <jamesroyals3@gmail.com>
2017-10-17 12:50:08 -0400

    finished out the routes for the items controller

--- routes/ItemsController.js
+++ routes/ItemsController.js
@@ -2,6 +2,29 @@ const express = require('express')
 const router = express.Router({ mergeParams: true })
 const { User, Pantry, Item } = require('../db/schema')
 
+router.get('/', async (req, res) => {
+    try{
+        const user = await User.findById(req.params.userId)
+        const setting = user.pantry.id(req.params.pantryId)
+        const item = setting.items
+        res.json(item)
+    } catch (err) {
+        res.send(err)
+    }
+})
+
+router.post('/', async (req, res) => {
+    try{
+      const newItem = req.body
+      const user = await User.findById(req.params.userId)
+      const pantry = user.pantry.id(req.params.pantryId)
+      pantry.items.push(newItem)
+      const saved = await user.save()
+      res.json(saved)
+    } catch (err)  {
+      res.json(err)
+    }
+  })
 
 router.get('/:id', async (req, res) => {
     try {
@@ -31,6 +54,16 @@ router.patch('/:id', async (req, res) => {
         res.send(err)
     }
    })
+
+   router.delete('/:id', async (req, res) => {
+    
+      const user = await User.findById(req.params.userId)
+      const pantry = user.pantry.id(req.params.pantryId)
+      pantry.items.id(req.params.id).remove()
+      
+      const saved = await user.save()
+      res.json(saved)
+    })
 
 
 
