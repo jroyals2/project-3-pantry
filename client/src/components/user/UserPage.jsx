b6b22adfbb4bb834ba9a784fc889da5db1604dbc
jroyals2 <jamesroyals3@gmail.com>
2017-10-17 10:34:54 -0400

    added the delete route for the Pantry

--- client/src/components/user/UserPage.jsx
+++ client/src/components/user/UserPage.jsx
@@ -6,55 +6,62 @@ import EditPantry from '../pantry/EditPantry'
 
 class UserPage extends Component {
     state = {
-        user : {
-            userName:'',
+        user: {
+            userName: '',
             password: '',
             pantry: []
         },
         admin: false
     }
 
-    async componentWillMount(){
+    async componentWillMount() {
         const { userId } = this.props.match.params
         const res = await axios.get(`/api/users/${userId}`)
-        this.setState({user: res.data})
+        this.setState({ user: res.data })
     }
 
     toggleAdmin = () => {
-        this.setState({admin: !this.state.admin})
+        this.setState({ admin: !this.state.admin })
+    }
+
+    deletePantry = async (event) => {
+        event.preventDefault()
+        const { userId } = this.props.match.params
+        const id = event.target.value
+        console.log(id)
+        const res = await axios.delete(`/api/users/${userId}/pantry/${id}`)
+        this.setState({ user: res.data })
     }
 
     createNewPantry = async (newPantry) => {
-    const { userId } = this.props.match.params
-    const res = await axios.post(`/api/users/${userId}/pantry`, {
-    "pantry": newPantry
-    })
-    this.setState({user: res.data})
-  }
-  updatePantry = async (pantryId) => {
-    const { userId } = this.props.match.params
-    const id = pantryId
+        const { userId } = this.props.match.params
+        const res = await axios.post(`/api/users/${userId}/pantry`, {
+            "pantry": newPantry
+        })
+        this.setState({ user: res.data })
+    }
+    updatePantry = async (pantryId) => {
+        const { userId } = this.props.match.params
+        const id = pantryId
+        const clonedUser = { ...this.state.user }
+        const pantry = clonedUser.pantry.find(i => i._id === pantryId)
+        const res = await axios.patch(`/api/users/${userId}/pantry/${id}`, {
+            pantry: pantry
+        })
+        this.setState({ user: res.data })
+    }
 
-    const clonedUser = {...this.state.user}
-    const pantry = clonedUser.pantry.find(i => i._id === pantryId)
-
-    const res = await axios.patch(`/api/users/${userId}/pantry/${id}`, {
-      pantry: pantry
-    })
-    this.setState({user: res.data})
-  }
-
-  handleChange = (event, pantryId) => {
-    const attribute = event.target.name
-    const clonedUser = {...this.state.user}
-    const pantry = clonedUser.pantry.find(i => i._id === pantryId)
-    console.log(pantry)
-    pantry[attribute] = event.target.value
-    this.setState({user: clonedUser})
-  }
+    handleChange = (event, pantryId) => {
+        const attribute = event.target.name
+        const clonedUser = { ...this.state.user }
+        const pantry = clonedUser.pantry.find(i => i._id === pantryId)
+        console.log(pantry)
+        pantry[attribute] = event.target.value
+        this.setState({ user: clonedUser })
+    }
 
 
-    
+
     render() {
         return (
             <div>
@@ -62,19 +69,20 @@ class UserPage extends Component {
                 {this.state.user.pantry.map((pantry) => {
                     return (
                         <div key={pantry._id}>
-                        <Link to={`/user/${this.state.user._id}/pantry/${pantry._id}`}>{pantry.pantryName}</Link>
-                        <div><button onClick={this.toggleAdmin}>{this.state.admin ? 'Hide' : 'Edit this Pantry'}</button></div>
-                        {this.state.admin ? <EditPantry 
-                        handleChange={this.handleChange}
-                        updatePantry={this.updatePantry}
-                        pantry={pantry}/> : `Don't like this pantry? Edit it !` }
-                        <hr/>
+                            <Link to={`/user/${this.state.user._id}/pantry/${pantry._id}`}>{pantry.pantryName}</Link>
+                            <button value={pantry._id} onClick={this.deletePantry}>Delete</button>
+                            <div><button onClick={this.toggleAdmin}>{this.state.admin ? 'Hide' : 'Edit this Pantry'}</button></div>
+                            {this.state.admin ? <EditPantry
+                                handleChange={this.handleChange}
+                                updatePantry={this.updatePantry}
+                                pantry={pantry} /> : `Don't like this pantry? Edit it !`}
+                            <hr />
                         </div>
                     )
                 })}
-                <PantryForm 
-                pantry={this.state.pantry}
-                createNewPantry={this.createNewPantry}
+                <PantryForm
+                    pantry={this.state.pantry}
+                    createNewPantry={this.createNewPantry}
                 />
             </div>
         );
